b76e9faad917668416dcd1322d6770571c63e508
Ed Scheinerman <ers@jhu.edu>
2018-04-07 17:37:19 -0400

    Added bare-bones testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,11 @@ using FlexLinearAlgebra
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+v = FlexConvert([4,5,6])
+@test v+v == 2v
+@test v-v == FlexVector(1:3)
+@test dot(v,v) == 77
+v[2] = 4
+v[3] = 4
+@test v == 4*FlexOnes(1:3)
+@test v[-1] == 0
