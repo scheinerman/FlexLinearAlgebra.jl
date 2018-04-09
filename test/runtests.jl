71235c31c5388b23442882cf9f9e53b7af8e8edc
Ed Scheinerman <ers@jhu.edu>
2018-04-09 16:49:48 -0400

    More testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using FlexLinearAlgebra
 using Base.Test
 
-# write your own tests here
 v = FlexConvert([4,5,6])
 @test v+v == 2v
 @test v-v == FlexVector(1:3)
@@ -14,10 +13,24 @@ v[3] = 4
 x = collect(1:5)
 v = FlexConvert(x)
 @test Vector(v) == x
+@test Set(keys(v)) == Set(1:5)
+@test sum(values(v)) == sum(v)
+@test keytype(v) == Int
+@test valtype(0. * v) == Float64
+@test length(v) == 5
+@test haskey(v,1)
+@test (-v) + v == 0v
 
+v = FlexOnes(Int,1:6)
+delete_entry!(v,5)
+delete_entry!(v,5)
+@test sum(v) == 5
 
 M = FlexConvert(eye(Int,3))
 @test M[1,1]==1
+S = Set(1:3)
+@test Set(row_keys(M)) == S
+@test Set(col_keys(M)) == S
 
 A = [1 3; 4 5]
 B = [-1.0 3; 5 -1.5]
@@ -25,6 +38,13 @@ v = [2; 3]
 AA = FlexConvert(A)
 BB = FlexConvert(B)
 vv = FlexConvert(v)
+
+@test valtype(BB) == Float64
+S = Set(1:2)
+@test Set(row_keys(BB)) == S
+@test Set(col_keys(BB)) == S
+
+
 @test Vector(AA*vv) == A*v
 @test Matrix(AA*BB) == Matrix(A)*Matrix(B)
 @test (AA*BB)*vv == AA*(BB*vv)
@@ -33,10 +53,8 @@ AA[1,3] = 2
 @test size(AA) == (2,3)
 
 
-v = FlexOnes(Int,1:6)
-delete_entry!(v,5)
-delete_entry!(v,5)
-@test sum(v) == 5
+
+
 
 A = FlexOnes(Int,1:5,1:5)
 delete_row!(A,5)
